aa39414ed0f1584c865dca8a475a983b3dd551ea
Abhilash <theak.92@gmail.com>
2026-04-21 01:03:34 +0530

    WIP: grafana addition

--- INITIALIZEME.sh
+++ INITIALIZEME.sh
@@ -6,6 +6,7 @@ docker compose -f docker-compose.local.yml up -d
 sleep 6
 # initialize postgres with data
 sudo chown -R 1000:1000 ./data/kafka
+sudo chown -R 472:472 ./data/grafana
 
 # synchronizing progress
 sleep 15
